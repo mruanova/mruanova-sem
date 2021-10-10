9b1ed5c500cb3e09b2ac48326b81fe9ebe4d4dfb
Mauricio Ruanova <mruanova@gmail.com>
2021-10-10 13:03:46 -0500

    first-version

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# MruanovaSem
+# Mauricio Ruanova
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.24.
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'mruanova-sem';
+  title = 'Lead Software Engineer';
 }
