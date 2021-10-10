543edc0f722aa87ef8a4d33172da88e8c2eebe98
Mauricio Ruanova <mruanova@gmail.com>
2021-10-10 14:47:42 -0500

    mobile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  header = 'Mauricio Ruanova';
   title = 'Lead Software Engineer';
+  image = 'https://mruanova.com/mau.png';
+  linkedin = 'https://www.linkedin.com/in/mruanova/';
+  offshore = 'https://en.wikipedia.org/wiki/Offshoring';
+  nearshore = 'https://en.wikipedia.org/wiki/Outsourcing#Nearshoring';
+  agile = 'https://en.wikipedia.org/wiki/Agile_software_development';
+  saas = 'https://en.wikipedia.org/wiki/Software_as_a_service';
+  microservices = 'https://en.wikipedia.org/wiki/Microservices';
+  book = 'https://books.google.com/books/about/Estandarización_y_mejora_de_procesos_y.html?id=ipI3HQAACAAJ';
+  papers = 'https://link.springer.com/chapter/10.1007/11499053_28/';
+  frontEnd = 'https://en.wikipedia.org/wiki/Front-end_web_development';
+  softwareEngneer = 'https://en.wikipedia.org/wiki/Software_engineering';
+  deployments = 'https://en.wikipedia.org/wiki/Software_deployment';
+  releases = 'https://en.wikipedia.org/wiki/Software_release_life_cycle';
+  softSkills = 'https://en.wikipedia.org/wiki/Soft_skills';
+  devops = 'https://en.wikipedia.org/wiki/DevOps';
+  standUp = 'https://en.wikipedia.org/wiki/Stand-up_meeting';
+  solid = 'https://en.wikipedia.org/wiki/SOLID';
+  dependencyInjection = 'https://en.wikipedia.org/wiki/Dependency_injection';
+  teamLeade = 'https://en.wikipedia.org/wiki/Team_leader';
+  sprintPlanning = 'https://en.wikipedia.org/wiki/Scrum_(software_development)#Sprint_planning';
+  retro = 'https://en.wikipedia.org/wiki/Scrum_(software_development)#Sprint_retrospective';
+  grooming = 'https://en.wikipedia.org/wiki/Scrum_(software_development)#Backlog_refinement';
+  demo = 'https://en.wikipedia.org/wiki/Scrum_(software_development)#Sprint_review';
+  softwareArchitecture = 'https://en.wikipedia.org/wiki/Software_architecture';
+  serverless = 'https://en.wikipedia.org/wiki/Serverless_computing';
+  net = 'https://en.wikipedia.org/wiki/.net';
+  codeReview = 'https://en.wikipedia.org/wiki/Code_review';
 }
